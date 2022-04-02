2191633a1af627d8bb7886080026666e8593c4a2
Vurmaz <sadikvurmaz@gmail.com>
2022-04-02 14:50:12 +0300

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react"
 import { FaGithub } from 'react-icons/fa'
 function Footer() {
     return(
-        <footer className="bg-3 bottom-0 h-9vh flex items-center justify-center gap-4">  
+        <footer className="bg-3 bottom-0 h-16 flex items-center justify-center gap-4">  
             <h2 className="text-xl rm-text4">Created by Vurmaz</h2>
             <FaGithub fontSize='3rem' className="rm-text4 cursor-pointer hover-git"></FaGithub>
         </footer>
